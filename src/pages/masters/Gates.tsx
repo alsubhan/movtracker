009543527b10c3557a8b78d91dc9e429b25347df
SAS <25459313+alsubhan@users.noreply.github.com>
2025-04-03 18:21:30 -0400

    added Gates to the Layout

--- src/pages/masters/Gates.tsx
+++ src/pages/masters/Gates.tsx
@@ -249,6 +249,7 @@ const Gates = () => {
   };
 
   return (
+    <Layout>
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-bold tracking-tight">Gates Management</h2>
@@ -582,6 +583,7 @@ const Gates = () => {
         </TabsContent>
       </Tabs>
     </div>
+    </Layout>
   );
 };
 
